2126781ef0958d00da73dc489153bd987939ca37
Жаров Андрей <rougik3000@gmail.com>
2026-02-27 19:00:39 +0300

    Гитигнор + импорты

--- src/main/java/ru/test/document_service/dto/DocumentDetailsResponse.java
+++ src/main/java/ru/test/document_service/dto/DocumentDetailsResponse.java
@@ -2,9 +2,7 @@ package ru.test.document_service.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import ru.test.document_service.domain.enums.DocumentStatus;
 
-import java.time.Instant;
 import java.util.List;
 
 /**
